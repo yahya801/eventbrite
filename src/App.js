c00cb8ed4fc3bf89045dd715158837cc6143ac37
yahya801 <53046561+yahya801@users.noreply.github.com>
2020-02-22 18:37:19 +0500

    22

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-   <h1>Hello Worl</h1>
+   <h1> Hello Worlddddd</h1>
     </div>
   );
 }

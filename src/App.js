846958406ad06327afd5c9524a44fa261c272549
yahya801 <53046561+yahya801@users.noreply.github.com>
2020-02-24 17:21:00 +0500

    new template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from "react";
+import Header from './Mainpage/header';
 
 
-function App() {
-  return (
+
+export class App extends Component {
+  render() {
+    return (
     <div className="App">
-   <h1> Hello Worlddddd</h1>
+      <Header/>
+      
     </div>
   );
+}
 }
 
 export default App;
